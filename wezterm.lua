0f1827cc073a9ddb2a24772f2cbd46b9f73fb913
Reima Kokko <reima.kokko@valolink.fi>
2024-11-17 12:23:15 +0200

    opacity

--- wezterm.lua
+++ wezterm.lua
@@ -25,6 +25,8 @@ end)
 
 local config = wezterm.config_builder()
 
+config.window_background_opacity = 0.80
+
 config.hide_tab_bar_if_only_one_tab = true
 
 config.disable_default_mouse_bindings = true
